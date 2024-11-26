39950a687bfebb5379d5ed250e8f9fe57e992ab2
Milan <133103282+milannal1m@users.noreply.github.com>
2024-11-26 18:32:42 +0100

    Prettier

--- main.c
+++ main.c
@@ -1,33 +1,29 @@
-
-
 #include <stdio.h>
 #include <stdint.h>
 #include <esp32c3_reg.h>
 #include "esp_intr_alloc.h"
 
 #define LEDS 16
-uint8_t pixels[LEDS*3]; // Komponenten für Grün, Rot und Blau je LED
-
-
-volatile uint32_t* gpio_out_reg = (volatile uint32_t*) GPIO_OUT_REG;
-volatile uint32_t* gpio_in_reg = (volatile uint32_t*) GPIO_IN_REG;
+uint8_t pixels[LEDS*3]; // 3  color channels per LED
 
 void delay(uint32_t us)
 {
-	//delay of us* microseconds
+    /*
+        Waits us microseconds.
+    */
 
 	asm volatile(
-		"1: \n\t"
-		"beq %0,zero,3f \n\t"
-		"li t0,38 \n\t"
-		"2: \n\t"
-		"addi t0,t0,-1 \n\t"
-		"bne t0, zero, 2b \n\t"
-		"addi %0,%0,-1 \n\t"
-		"j 1b \n\t"
-		"3: \n\t"
+		"1:                                     \n\t"
+		"beq %0,zero,3f                         \n\t"   // if us == 0 -> 3
+		"li t0,38                               \n\t"   // t0 = 38
+		"2:                                     \n\t"
+		"addi t0,t0,-1                          \n\t"   // t0--
+		"bne t0, zero, 2b                       \n\t"   // if t0 != 0 -> 2
+		"addi %0,%0,-1                          \n\t"   // us--
+		"j 1b                                   \n\t"   // jump to 1
+		"3:                                     \n\t"
 		:
-		: "r"(us)
+		: "r"(us)                                       // %0: adress of us
 		:"t0"
 	);
 
@@ -35,64 +31,67 @@ void delay(uint32_t us)
 
 void display()
 {
-	//Displays pixels onto the LED
+	/*
+        Writes every bit of every number of the pixel data to GPIO1.
+        Writes HIGH to GPIO1 for 600ns if the bit is 1.
+        Writes LOW to GPIO1 for 300ns if the bit is 0.
+        After Writing a Bit waits 650ns, if the bit was 1 and waits 950ns if the bit was 0.
+        In the end waits for 50us to set the LED strip.
+    */
 
 	int factor = 1;
 
-	for(int i = 0; i<LEDS*3; i++){
+	for(int i = 0; i<LEDS*3; i++){                      // For every channel of every LED
 
-
-		for (int n = 7; n >= 0; n--) {
-			// Überprüfen, ob das i-te Bit gesetzt ist
-			if (pixels[i] & (1 << n)) {
-				factor = 2;
-			}else{
+		for (int n = 7; n >= 0; n--) {                  // For every bit of every number
+			                                        
+			if (pixels[i] & (1 << n)) {                 // If bit n of number i is 1
+				factor = 2;                             // Set factor to 2 else 1
+			}else{                                  
 				factor = 1;
 			}
 
 			asm volatile (
 
 				//GPIO2 HIGH
-
-				"li t0,2						\n\t"
-				"lw t1, gpio_out_reg			\n\t"
-				"or t2,t1,t0 					\n\t" // *gpio_out_reg |= (1 << 1)
-				"sw t2, 0(t1)					\n\t"
+				"li t0,2						\n\t"   // t0 = 2
+				"lw t1, gpio_out_reg			\n\t"   // t1 = *gpio_out_reg
+				"or t2,t1,t0 					\n\t"   // *gpio_out_reg |= (2)
+				"sw t2, 0(t1)					\n\t"   // store gpio_out_reg
 
 				//Warteschleife HIGH
-				"li t0, 10                  	\n\t" //t0 = 720
-				"mul t2,t0,%1					\n\t" //t2 = 720*factor
-				"add t3,t2,zero 				\n\t" // t3 = t2
+				"li t0, 10                  	\n\t"   // t0 = 10
+				"mul t2,t0,%0					\n\t"   // t2 = 10*factor
+				"add t3,t2,zero 				\n\t"   // t3 = t2
 
 				"1:                  			\n\t"
-				"addi t2,t2,-1                  \n\t" // t2--
-				"bne t2,zero,1b                 \n\t" // if t2 != 0 -> 1
+				"addi t2,t2,-1                  \n\t"   // t2--
+				"bne t2,zero,1b                 \n\t"   // if t2 != 0 -> 1
 
 				//GPIO2 LOW
-				"li t0,2						\n\t"
-				"lw t1,gpio_out_reg				\n\t"
-				"not t0,t0 						\n\t"
-				"add t2,t1,t0 					\n\t" // *gpio_out_reg &= ~(1 << 1)
-				"sw t2, 0(t1)					\n\t" 
+				"li t0,2						\n\t"   // t0 = 2
+				"lw t1,gpio_out_reg				\n\t"   // t1 = *gpio_out_reg
+				"not t0,t0 						\n\t"   // t0 = ~t0
+				"add t2,t1,t0 					\n\t"   // *gpio_out_reg &= ~(2)
+				"sw t2, 0(t1)					\n\t"   // store gpio_out_reg
 
 				//Wartschleife LOW
-				"li t4, 39					\n\t" // t4 = 3000
-				"sub t3,t4,t3					\n\t" // t3 = 3000 - 720*factor
+				"li t4, 39					    \n\t"   // t4 = 39
+				"sub t3,t4,t3					\n\t"   // t3 = 39 - 10*factor
 
 				"2:                  			\n\t"
-				"addi t3,t3,-1                  \n\t" // t3--
-				"bne t3,zero,2b                 \n\t" // if t3 != 0 -> 2
+				"addi t3,t3,-1                  \n\t"   // t3--
+				"bne t3,zero,2b                 \n\t"   // if t3 != 0 -> 2
 
 				:
 				:    
-				"r" (gpio_out_reg), 	// %0: Adresse von gpio_out_reg
-				"r" (factor)           // %1: Adresse von factor 
+				"r" (factor)                            // %0: adress of factor
 				: "t0", "t1", "t2","t3","t4"
 			);
 		}
 
 	}
-	delay(50);
+	delay(50);                                          // Wait 50us    
 
 	
 
@@ -100,42 +99,50 @@ void display()
 
 void config_timer()
 {
-	volatile uint32_t* timg_t0_config_reg = (volatile uint32_t*) TIMG_T0CONFIG_REG;
-	volatile uint32_t* timg_t0alarmlo_reg = (volatile uint32_t*) TIMG_T0ALARMLO_REG;
+    /*
+    Configures the Timer
+    */
 
-	*timg_t0_config_reg |= (5 << 16); // DIVIDER
-	*timg_t0_config_reg |= (1 << 10); // ALARMEN
+	*TIMG_T0CONFIG_REG |= (5 << 16);                    // Sets the Divider of the Timer to 5
+	*TIMG_T0CONFIG_REG |= (1 << 10);                    // Enables the Alarm of the Timer
 
-	*timg_t0alarmlo_reg = 1048576;
+	*TIMG_T0ALARMLO_REG = 1048576;                      // Sets the Alarm Value of the Timer to 1048576
 
-	*timg_t0_config_reg |= (1 << 31);
+	*TIMG_T0ALARMLO_REG |= (1 << 31);                   //Enables the Timer
 
 }
 
 void readSensor()
 {
-	// Reads the Echo Sensor
+	/*
+    Reads the Echo Sensor
+    */
 
 	config_timer();
 
-	*gpio_out_reg |= (1 << 2);
-	delay(1000000);
-	*gpio_out_reg &= ~(1 << 2);
+    long time1 = 0;                                     // Variables to save the time at the rising and falling edge of the Echo signal
+    long time2 = 0;
+    long timediff = 0;
 
-	*TIMG_T0UPDATE_REG |= (1 << 31);
 
-	long time1 = 0;
+	*GPIO_OUT_REG |= (1 << 2);                          // Set GPIO2 to HIGH
+	delay(1000000);                                     // Wait 1s eigentlich 10us
+	*GPIO_OUT_REG &= ~(1 << 2);                         // Set GPIO2 to LOW
 
-	while(*gpio_in_reg & (1<<3)){
+	while(!(*GPIO_IN_REG & (1<<3))){                    // While the Echo is LOW
+    }
+
+	while(*GPIO_IN_REG & (1<<3)){                       // While the Echo is HIGH
 		if(time1 != 0){
-			time1 = *TIMG_T0LO_REG;
+            *TIMG_T0UPDATE_REG |= (1 << 31);            // Allows the Counter of the Timer to be read
+			time1 = *TIMG_T0LO_REG;                     // Saves the time at the rising edge of the Echo signal
 		}
 	}
 
-	*TIMG_T0UPDATE_REG |= (1 << 31);
-	long time2 = *TIMG_T0LO_REG;
+	*TIMG_T0UPDATE_REG |= (1 << 31);                    // Allows the Counter of the Timer to be read
+	time2 = *TIMG_T0LO_REG;                             // Saves the time at the falling edge of the Echo signal
 
-	long timediff = time2-time1;
+	timediff = time2-time1;                             // Calculates the time between the rising and falling edge of the Echo signal
 
 	printf("%li\n\n",timediff);
 }
@@ -151,9 +158,7 @@ void loop()
 void app_main(void)
 {
 	
-    volatile uint32_t* gpio_enable_reg = (volatile uint32_t*) GPIO_ENABLE_REG;
-	
-    *gpio_enable_reg |= 0b110;  // enable GPIO1 output
+    *GPIO_ENABLE_REG |= 0b110;  // enable GPIO1 output
 
 	for (int i = 0; i < 15; i++) {
     	pixels[i] = 150;
